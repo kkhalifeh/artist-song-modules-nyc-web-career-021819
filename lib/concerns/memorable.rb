d273cc8964b6d8cfe4671af5aaf4402f82e0fea5
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-02-25 23:11:19 -0500

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,24 @@
+require 'pry'
+module Memorable
+
+  module InstanceMethods
+
+    def initialize
+      self.class.all << self
+    end 
+
+  end
+
+  module ClassMethods
+
+    def count
+      self.all.count
+    end
+
+    def reset_all
+      self.all.clear
+    end
+
+  end
+
+end
---
+++ lib/concerns/paramable.rb
@@ -0,0 +1,11 @@
+require 'pry'
+
+module Paramable
+  module InstanceMethods
+
+    def to_param
+      self.name.downcase.gsub(' ', '-')
+    end
+  end
+
+end
